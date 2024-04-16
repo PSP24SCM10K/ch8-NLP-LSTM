ca329eca682eb98649ebc2f3e459d36e7698eb30
pkumbhar1 <pkumbhar1@hawk.iit.edu>
2024-04-16 13:45:18 -0500

    update

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ print (my_sentence2, ' : ', predict_sentiment(my_sentence2))
 
 @app.route("/")
 def entry():
-    return "Wellcome!"
+    return "Welcome Prajakta!"
     
 # build a route or HTTP endpoint
 @app.route('/hello')
